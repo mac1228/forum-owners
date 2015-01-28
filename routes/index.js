411dc98c79831d69065c06c6a166f3d608dbf6b8
unknown <micurd@MICURDZ230.redmond.corp.microsoft.com>
2015-01-28 13:01:38 -0800

    added pasword validation

--- routes/index.js
+++ routes/index.js
@@ -173,7 +173,7 @@ router.get('/timeline', function(req, res, next) {
 });
 
 router.post('/post',function(req,res){
-   console.log(req.body.username) //you will get your data in this as object.
+   console.log(req.body.username); //you will get your data in this as object.
 })
 
 module.exports = router;
