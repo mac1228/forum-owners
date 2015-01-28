3363bb09c8d3cf46967d877cea61f90eb13f3398
unknown <micurd@MICURDZ230.redmond.corp.microsoft.com>
2015-01-28 09:05:52 -0800

    added links

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var homedata = require('../homepage.json');
+var page;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,13 +9,17 @@ router.get('/', function(req, res, next) {
   homearray = homedata.home;
   res.render('index', { 
   	title: 'Welcome to Forum Owners',
-  	home : homearray
+  	homeblocks : homearray,
+  	page : 'home'
   });
 });
 
 /* GET leaderboard page. */
 router.get('/leaderboards', function(req, res, next) {
-  res.render('index', { title: 'Leaderboard' });
+  res.render('index', { 
+  	title: 'Leaderboard',
+  	page : 'leaderboards' 
+  });
 });
 
 /* GET forum health page. */
@@ -26,93 +31,145 @@ router.get('/health', function(req, res, next) {
   }
   res.render('index', { 
     title: 'Forum Health',
-    user: username 
+    user: username,
+    page : 'health' 
   });
 });
 
 /* GET question aggregator page. */
 router.get('/questions', function(req, res, next) {
-  res.render('index', { title: 'Question Aggregator' });
+  res.render('index', { 
+  	title: 'Question Aggregator',
+  	page : 'questions' 
+  });
 });
 
-/* GET add players page. */
-router.get('/addplayers', function(req, res, next) {
-  res.render('index', { title: 'Add Players' });
+/* GET Sign up page. */
+router.get('/signup', function(req, res, next) {
+  res.render('index', { 
+  	title: 'Create Profile',
+  	page : 'signup' 
+  });
 });
 
 /* GET add players page. */
 router.get('/players', function(req, res, next) {
-  res.render('index', { title: 'List of Players' });
+  res.render('index', { 
+  	title: 'List of Players',
+  	page : 'players' 
+  });
 });
 
 /* GET profile page. */
 router.get('/profile', function(req, res, next) {
-  res.render('index', { title: 'Profile' });
+  res.render('index', { 
+  	title: 'Profile',
+  	page: 'profile' 
+  });
 });
 
 /* GET tournament page. */
 router.get('/tournament', function(req, res, next) {
-  res.render('index', { title: 'Tournament' });
+  res.render('index', { 
+  	title: 'Tournament',
+  	page: tournament 
+  });
 });
 
 /* GET teams page. */
 router.get('/teams', function(req, res, next) {
-  res.render('index', { title: 'Teams' });
+  res.render('index', { 
+  	title: 'Teams', 
+  	page: 'teams'
+  });
 });
 
 /* GET challenges page. */
 router.get('/challenges', function(req, res, next) {
-  res.render('index', { title: 'Challenges' });
+  res.render('index', { 
+  	title: 'Challenges',
+  	page: 'challenges' 
+  });
 });
 
 /* GET List of Achievements page. */
 router.get('/achievements', function(req, res, next) {
-  res.render('index', { title: 'List of Achievements' });
+  res.render('index', { 
+  	title: 'List of Achievements',
+  	page: 'achievements' 
+  });
 });
 
 /* GET Forum Owner of the Week page. */
 router.get('/forumowner', function(req, res, next) {
-  res.render('index', { title: 'Forum Owner of the Week' });
+  res.render('index', { 
+  	title: 'Forum Owner of the Week',
+  	page: 'forumowner' 
+  });
 });
 
 /* GET Canned Responses page. */
 router.get('/cannedresponses', function(req, res, next) {
-  res.render('index', { title: 'Canned Responses' });
+  res.render('index', { 
+  	title: 'Canned Responses',
+  	page: 'cannedresponses' 
+  });
 });
 
 /* GET Tracked Forums page. */
 router.get('/trackedforums', function(req, res, next) {
-  res.render('index', { title: 'Tracked Forums' });
+  res.render('index', { 
+  	title: 'Tracked Forums',
+  	page: 'trackedforums' 
+  });
 });
 
 /* GET Forum Owner Schedule page. */
 router.get('/schedule', function(req, res, next) {
-  res.render('index', { title: 'Forum Owner Schedule' });
+  res.render('index', { 
+  	title: 'Forum Owner Schedule',
+  	page: 'schedule' 
+  });
 });
 
 /* GET How this works page. */
 router.get('/howthisworks', function(req, res, next) {
-  res.render('index', { title: 'How this works' });
+  res.render('index', { 
+  	title: 'How this works',
+  	page: 'howthisworks' 
+  });
 });
 
 /* GET Roles and Responsibilities page. */
 router.get('/responsibilities', function(req, res, next) {
-  res.render('index', { title: 'Roles and Responsibilities' });
+  res.render('index', { 
+  	title: 'Roles and Responsibilities',
+  	page: 'responsibilities' 
+  });
 });
 
 /* GET Prize of the Month page. */
 router.get('/prizes', function(req, res, next) {
-  res.render('index', { title: 'Prize of the Month!' });
+  res.render('index', { 
+  	title: 'Prize of the Month!',
+  	page: 'prizes' 
+  });
 });
 
 /* GET Log in/Sign up page. */
 router.get('/login', function(req, res, next) {
-  res.render('index', { title: 'Log in/Sign up' });
+  res.render('index', { 
+  	title: 'Log in/Sign up',
+  	page: 'login' 
+  });
 });
 
 /* GET Log in/Sign up page. */
 router.get('/timeline', function(req, res, next) {
-  res.render('index', { title: 'Timeline' });
+  res.render('index', { 
+  	title: 'Timeline',
+  	page: 'timeline' 
+  });
 });
 
 module.exports = router;
