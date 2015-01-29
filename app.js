e0bc601de2cd9358dcf353b3c96d274c743cc584
unknown <micurd@micurdx1.redmond.corp.microsoft.com>
2015-01-28 21:27:34 -0800

    added gitignore

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var azure = require('azure-storage');
 
 var routes = require('./routes/index');
 
